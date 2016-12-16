538f3b1753a9e8b3b3808068c2461a1a72e308e9
acarrara <alessandro.carrara@gmail.com>
2016-12-16 15:32:48 +0100

    feat(counter): add count for straightening visitors

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ var app = express();
 var path = require('path');
 var port = process.env.PORT || 8080;
 
-app.post('/inc', function(req, res) {
-    console.log('inc!');
-    res.sendStatus(200);
-});
+var total = 0;
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use('/', express.static(path.join(__dirname, '/')));
 
-app.listen(port, function () {
+io.on('connection', function(socket){
+    io.emit('total', total);
+    socket.on('inc', function(msg){
+      total++;
+      io.emit('total', total);
+  });
+});
+
+http.listen(port, function () {
   console.log('express listening on port ' + port + '!')
 })
