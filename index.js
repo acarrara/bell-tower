993289d303c1aca885379817c94679d53174e84a
acarrara <alessandro.carrara@gmail.com>
2016-12-16 16:55:55 +0100

    feat(db): add postgres as persistence

--- index.js
+++ index.js
@@ -3,21 +3,51 @@ var app = express();
 var path = require('path');
 var port = process.env.PORT || 8080;
 
+var pg = require('pg');
+pg.defaults.ssl = true;
+
+var Pool = pg.Pool;
+var pool = new Pool(config);
+var config = {
+  database: process.env.DATABASE_URL
+};
 var total = 0;
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+process.on('unhandledRejection', function(e) {
+  console.log(e.message, e.stack)
+})
+
 app.use('/', express.static(path.join(__dirname, '/')));
+
+pool.connect(function(err, client, done) {
+  if (err) throw err;
+  client.query('SELECT * FROM straighten;')
+    .on('row', function(row) {
+      total = row.total;
+    });
+    done();
+});
 
 io.on('connection', function(socket){
     io.emit('total', total);
     socket.on('inc', function(msg){
       total++;
+
+      pool.connect(function(err, client, done) {
+        if (err) throw err;
+        client.query('UPDATE straighten SET total=$1;', [total], function(err) {
+          if (err) throw(err);
+          done();
+        });
+      });
+
       io.emit('total', total);
   });
 });
 
 http.listen(port, function () {
   console.log('express listening on port ' + port + '!')
-})
+});
